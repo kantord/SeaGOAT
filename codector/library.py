ed3e68a8ac92c2d1ba515932288d0ba08a048e57
Daniel Kantor <git@daniel-kantor.com>
2023-06-20 19:32:08 +0000

    ci: test code formatting

--- codector/library.py
+++ codector/library.py
@@ -2,19 +2,20 @@
     This module allows you to use Codector as a library
 """
 
+
 class Codector:
     """
-        A search engine for a code repository
+    A search engine for a code repository
     """
 
     def version(self):
         """
-            Returns the version of the library
+        Returns the version of the library
         """
         return "0.1.0"
 
     def is_ready(self):
         """
-            Returns true if the library is ready
+        Returns true if the library is ready
         """
         return False
--- tests/test_library.py
+++ tests/test_library.py
@@ -1,8 +1,10 @@
 from codector.library import Codector
 
+
 def test_version_method_returns_poetry_package_version():
     codector = Codector()
     assert codector.version() == "0.1.0"
+
 
 def test_has_an_is_ready_method_that_returns_false():
     codector = Codector()
