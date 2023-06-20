245177a2fc0885ebfbedf53c161115a88fb4a5ed
Daniel Kantor <git@daniel-kantor.com>
2023-06-20 17:22:14 +0200

    ci: add pylint for code style tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,6 +1,9 @@
 from codector.library import Codector
 
-def test_can_initialize_codector():
+def test_version_method_returns_poetry_package_version():
     codector = Codector()
-    assert codector is not None
+    assert codector.version() == "0.1.0"
 
+def test_has_an_is_ready_method_that_returns_false():
+    codector = Codector()
+    assert not codector.is_ready()
