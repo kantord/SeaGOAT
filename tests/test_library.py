3bac9dda7d48acf164710b864232c95ac0270ee2
Daniel Kantor <git@daniel-kantor.com>
2023-06-20 21:54:57 +0000

    feat: allow returning list of files

--- tests/test_library.py
+++ tests/test_library.py
@@ -4,3 +4,20 @@ from codector.library import Codector
 def test_returns_file_list_1(repo):
     codector = Codector(repo.working_dir)
     assert set(codector.files()) == {"file1.md", "file2.py", "file3.py", "file4.js"}
+
+
+def test_returns_file_list_2(repo):
+    codector = Codector(repo.working_dir)
+    repo.add_file_change_commit(
+        file_name="new_file.cpp",
+        contents="#include <iostream>",
+        author=repo.actors["John Doe"],
+        commit_message="Initial commit for C++ file",
+    )
+    assert set(codector.files()) == {
+        "file1.md",
+        "file2.py",
+        "file3.py",
+        "file4.js",
+        "new_file.cpp",
+    }
