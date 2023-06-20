6b5c02cceb3cdb6f53820ef712204d74ea314f22
Daniel Kantor <git@daniel-kantor.com>
2023-06-20 19:31:14 +0000

    ci: add pylint for code style tests

--- codector/library.py
+++ codector/library.py
@@ -1,3 +1,20 @@
-class Codector:
-    pass
+"""
+    This module allows you to use Codector as a library
+"""
 
+class Codector:
+    """
+        A search engine for a code repository
+    """
+
+    def version(self):
+        """
+            Returns the version of the library
+        """
+        return "0.1.0"
+
+    def is_ready(self):
+        """
+            Returns true if the library is ready
+        """
+        return False
