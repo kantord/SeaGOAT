f61e397708cf33d48dc867cab60e6d19c19adcbf
Daniel Kantor <git@daniel-kantor.com>
2023-06-23 22:21:56 +0200

    feat: allow querying results using chromadb

--- codector/engine.py
+++ codector/engine.py
@@ -11,8 +11,11 @@ from git.repo import Repo
 from gitdb.db.loose import os
 from tqdm import tqdm
 import appdirs
+import chromadb
+from chromadb.errors import IDAlreadyExistsError
 
 from codector.file import File
+from codector.result import Result
 
 
 IGNORED_BRANCHES = {"gh-pages"}
@@ -38,6 +41,7 @@ class Engine:
     A search engine for a code repository
     """
 
+    # pylint: disable=too-many-instance-attributes
     def __init__(self, path: str):
         """
         Initializes the library
@@ -50,6 +54,12 @@ class Engine:
         self._required_commits: Set[str] = set()
         self._last_analyzed_version_of_branch: Dict[str, str] = {}
         self._load_cache()
+        self.query_string = ""
+        self._results = []
+        self._chroma_client = chromadb.Client()
+        self._chroma_collection = self._chroma_client.create_collection(
+            name="code_data"
+        )
 
     def _get_cache_folder(self):
         cache_folder = self._get_cache_root() / self._get_project_hash()
@@ -140,6 +150,29 @@ class Engine:
 
         self._sort_files()
         self._write_cache()
+        self._create_vector_embeddings()
+
+    def _create_vector_embeddings(self):
+        for file in self._file_data.values():
+            full_path = Path(self.path) / file.path
+            if not full_path.exists():
+                continue
+            with open(full_path, "r", encoding="utf-8") as source_code_file:
+                file_content = source_code_file.read()
+
+            content = f"""
+                {file_content}
+                ###
+                {file.get_metadata()}
+            """
+            try:
+                self._chroma_collection.add(
+                    ids=[file.path],
+                    documents=[content],
+                    metadatas=[{"path": file.path}],
+                )
+            except IDAlreadyExistsError:
+                pass
 
     def top_files(self):
         return [self._file_data[path] for path in self._sorted_files]
@@ -148,3 +181,21 @@ class Engine:
         if path not in self._file_data:
             raise RuntimeError("File not found or not analyzed yet")
         return self._file_data[path]
+
+    def query(self, query: str):
+        self.query_string = query
+
+    def fetch(self):
+        chromadb_results = [
+            self._chroma_collection.query(query_texts=[self.query_string], n_results=5)
+        ]
+        metadatas = (
+            chromadb_results[0]["metadatas"][0]
+            if chromadb_results[0]["metadatas"]
+            else None
+        )
+
+        self._results = [str(item["path"]) for item in metadatas] if metadatas else []
+
+    def get_results(self):
+        return [Result(path) for path in self._results]
---
+++ codector/result.py
@@ -0,0 +1,4 @@
+# pylint: disable=too-few-public-methods
+class Result:
+    def __init__(self, path: str) -> None:
+        self.path = path
