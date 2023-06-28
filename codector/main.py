9e514eeacd305a97caa3888a665c86725168a4fd
Daniel Kantor <git@daniel-kantor.com>
2023-06-28 16:06:45 +0200

    feat: add click library for CLI

--- codector/main.py
+++ codector/main.py
@@ -1,9 +1,17 @@
-import sys
-
+import click
 from engine import Engine
 
 
-my_codector = Engine(sys.argv[1])
-my_codector.analyze_codebase()
-for file in my_codector.repository.top_files()[:1000]:
-    print(file)
+@click.command()
+@click.argument("repo_path")
+def analyze_codebase(repo_path):
+    """Analyze a codebase and print the top files"""
+    my_codector = Engine(repo_path)
+    my_codector.analyze_codebase()
+    for file in my_codector.repository.top_files()[:1000]:
+        print(file)
+
+
+if __name__ == "__main__":
+    # pylint: disable-next=no-value-for-parameter
+    analyze_codebase()
