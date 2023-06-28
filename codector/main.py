7b846e9f4029acc8810b8be3a374e91eaab26f06
Daniel Kantor <git@daniel-kantor.com>
2023-06-28 19:30:22 +0000

    feat: add simple interactive main command

--- codector/main.py
+++ codector/main.py
@@ -1,15 +1,39 @@
+# pylint: disable=too-few-public-methods
+
 import click
 from engine import Engine
+from prompt_toolkit import PromptSession
+from prompt_toolkit.validation import Validator
+from blessed import Terminal
+
+
+class RealTimeValidator(Validator):
+    def __init__(self, term):
+        self.term = term
+
+    def validate(self, document):
+        current_text = document.text
+        if current_text:
+            print(self.term.move_xy(0, 0) + self.term.clear_eol(), end="")
+            print(f"Query: {current_text}", end="")
+            print(self.term.move_xy(0, 2) + self.term.clear_eos(), end="")
+            print(f"mock results for '{current_text}', blah blah", end="")
 
 
 @click.command()
 @click.argument("repo_path")
 def analyze_codebase(repo_path):
-    """Analyze a codebase and print the top files"""
+    """Query your codebase using vector embeddings"""
     my_codector = Engine(repo_path)
     my_codector.analyze_codebase()
-    for file in my_codector.repository.top_files()[:1000]:
-        print(file)
+
+    term = Terminal()
+    with term.fullscreen():
+        session = PromptSession(validator=RealTimeValidator(term))
+        try:
+            session.prompt()
+        except EOFError:
+            pass
 
 
 if __name__ == "__main__":
