3323b2baf662682716b236491358e7bd09660268
Daniel Kantor <git@daniel-kantor.com>
2023-06-28 19:37:04 +0000

    feat: group results by file

--- codector/result.py
+++ codector/result.py
@@ -2,3 +2,7 @@
 class Result:
     def __init__(self, path: str) -> None:
         self.path = path
+        self.lines = set()
+
+    def add_line(self, line: int) -> None:
+        self.lines.add(line)
