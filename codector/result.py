af7b2b3a8f4bc0b583e4d9906814da76701e22a6
Daniel Kantor <git@daniel-kantor.com>
2023-06-30 19:18:13 +0000

    feat: prioritize including more files in the results

--- codector/result.py
+++ codector/result.py
@@ -6,12 +6,17 @@ class Result:
     def __init__(self, path: str, full_path: Path) -> None:
         self.path = path
         self.full_path = full_path
-        self.lines = set()
+        self._lines = set()
         self.line_texts = self._read_lines()
 
     def _read_lines(self):
         with open(self.full_path, encoding="utf-8") as source_code_file:
             return source_code_file.read().splitlines()
 
-    def add_line(self, line: int) -> None:
-        self.lines.add(line)
+    def add_line(self, line: int, vector_distance: float) -> None:
+        self._lines.add((line, vector_distance))
+
+    def get_lines(self):
+        best_score = min(self._lines, key=lambda x: x[1])[1]
+
+        return [line for line, score in self._lines if score <= best_score * 1.2]
