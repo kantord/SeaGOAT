c267a4e3a54cc7b100cf317c42edaaeeeed70f6f
Daniel Kantor <git@daniel-kantor.com>
2023-06-30 19:18:01 +0000

    refactor: extract ResultLine

--- codector/result.py
+++ codector/result.py
@@ -1,12 +1,20 @@
 # pylint: disable=too-few-public-methods
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Set
+
+
+@dataclass(frozen=True)
+class ResultLine:
+    line: int
+    vector_distance: float
 
 
 class Result:
     def __init__(self, path: str, full_path: Path) -> None:
-        self.path = path
-        self.full_path = full_path
-        self._lines = set()
+        self.path: str = path
+        self.full_path: Path = full_path
+        self._lines: Set[ResultLine] = set()
         self.line_texts = self._read_lines()
 
     def _read_lines(self):
@@ -14,9 +22,15 @@ class Result:
             return source_code_file.read().splitlines()
 
     def add_line(self, line: int, vector_distance: float) -> None:
-        self._lines.add((line, vector_distance))
+        self._lines.add(ResultLine(line, vector_distance))
 
     def get_lines(self):
-        best_score = min(self._lines, key=lambda x: x[1])[1]
+        best_score = min(
+            self._lines, key=lambda item: item.vector_distance
+        ).vector_distance
 
-        return [line for line, score in self._lines if score <= best_score * 1.2]
+        return [
+            result_line.line
+            for result_line in self._lines
+            if result_line.vector_distance <= best_score * 1.2
+        ]
