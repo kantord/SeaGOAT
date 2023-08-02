6d925433778a2986b07be3ff58bd8a937c90d555
Daniel Kantor <git@daniel-kantor.com>
2023-08-02 16:24:12 +0200

    refactor: use an iterator for limiting the results

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,13 +5,11 @@ from flask import json
 from seagoat import __version__
 from seagoat.cli import seagoat
 from seagoat.server import get_server_info_file
-from tests.conftest import pytest
 
 
 @pytest.mark.usefixtures("server")
-def test_integration_test_with_color(snapshot, repo, mocker):
+def test_integration_test_with_color(snapshot, repo, mocker, runner):
     mocker.patch("os.isatty", return_value=True)
-    runner = CliRunner()
     query = "JavaScript"
     result = runner.invoke(seagoat, [query, repo.working_dir])
 
@@ -20,9 +18,8 @@ def test_integration_test_with_color(snapshot, repo, mocker):
 
 
 @pytest.mark.usefixtures("server")
-def test_integration_test_without_color(snapshot, repo, mocker):
+def test_integration_test_without_color(snapshot, repo, mocker, runner):
     mocker.patch("os.isatty", return_value=True)
-    runner = CliRunner()
     query = "JavaScript"
     result = runner.invoke(seagoat, [query, repo.working_dir, "--no-color"])
 
@@ -34,24 +31,38 @@ def test_integration_test_without_color(snapshot, repo, mocker):
     "max_length",
     [
         (0),
+        (1),
         (2),
+        (20),
     ],
 )
-@pytest.mark.usefixtures("server")
-def test_limit_output_lenght(repo, mocker, max_length):
-    mocker.patch("os.isatty", return_value=True)
-    runner = CliRunner()
+def test_limit_output_lenght(
+    repo, max_length, mock_server_factory, mock_result_factory, runner
+):
+    fake_results = mock_result_factory(
+        [
+            ["hello.txt", ["foo", "bar", "baz"]],
+            ["foobar.py", ["def hello():", "    print('world')"]],
+            ["foobar2.py", ["def hola():", "    print('mundo')"]],
+            ["foobar3.py", ["def bonjour():", "    print('monde')"]],
+            ["foobar.fake", ["fn hello()", "    echo('world')"]],
+            ["foobar2.fake", ["fn hola()", "    echo('mundo')"]],
+            ["foobar3.fake", ["fn bonjour()", "    echo('monde')"]],
+        ]
+    )
+    print("🦗", fake_results)
+    mock_server_factory(fake_results)
     query = "JavaScript"
     result = runner.invoke(
         seagoat, [query, repo.working_dir, "--no-color", f"--max-results={max_length}"]
     )
 
     assert result.output.splitlines() == result.output.splitlines()[:max_length]
+    assert len(result.output.splitlines()) == min(max_length, 15)
     assert result.exit_code == 0
 
 
-def test_version_option():
-    runner = CliRunner()
+def test_version_option(runner):
     result = runner.invoke(seagoat, ["--version"])
 
     assert result.exit_code == 0
@@ -66,11 +77,11 @@ def test_version_option():
     ],
 )
 def test_server_is_not_running_error(mocker, repo_path, snapshot):
+    runner = CliRunner()
     server_info_file = get_server_info_file(repo_path)
     with open(server_info_file, "w", encoding="utf-8") as file:
         json.dump({"host": "localhost", "port": 345435, "pid": 234234}, file)
     mocker.patch("os.isatty", return_value=True)
-    runner = CliRunner()
     query = "JavaScript"
     result = runner.invoke(seagoat, [query, repo_path])
 
