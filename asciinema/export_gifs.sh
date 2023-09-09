c581e3660938c9f9bbb0654a0b039f891f73208b
Daniel Kantor <git@daniel-kantor.com>
2023-09-09 15:26:51 +0200

    docs: add titles to slideshow gif

--- asciinema/export_gifs.sh
+++ asciinema/export_gifs.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
+#
+#
+#
+
+
+
+title_slide() {
+    local title=$1
+    local subtitle=$2
+
+    local resolution="832x605"
+    local bgcolor="#222c34"
+    local fgcolor="#FFFFFF"
+
+    convert -size $resolution xc:"$bgcolor" \
+        -gravity Center \
+        -fill "$fgcolor" \
+        -pointsize 30 -annotate 0 "$title" \
+        -pointsize 35 -annotate +0+40 "$subtitle" \
+        GIF:-
+}
+
+
+title_gif() {
+    local delay=$1
+    local title=$2
+    local subtitle=$3
+
+    gifsicle --delay $delay <(title_slide "$title" "$subtitle")
+}
+
 
 cd "$(dirname "$0")"
 
@@ -13,12 +44,13 @@ done
 echo "All exported and copied!"
 
 
-# Generate slideshow
-
-gifsicle --loop --colors 256 \
-  ../assets/explain_what_you_want.gif \
-  ../assets/describe_what_you_are_looking_for.gif \
-  ../assets/incorporate_regex.gif \
+gifsicle  --loop --colors 256 \
+   <(title_gif 150 "Use Case 1" "Explain what you are trying to do" "title_1.gif") \
+   ../assets/explain_what_you_want.gif \
+   <(title_gif 150 "Use Case 2" "Describe what you are looking for" "title_1.gif") \
+   ../assets/describe_what_you_are_looking_for.gif \
+   <(title_gif 150 "Use Case 3" "Incorporate regular expressions" "title_1.gif") \
+   ../assets/incorporate_regex.gif \
   > ../assets/demo-slideshow.gif
 
 cp -f ../assets/demo-slideshow.gif ../docs/demo-slideshow.gif
