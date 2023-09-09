d829f2775de3e2f8855484ca12c6a9afd3f3ae10
Daniel Kantor <git@daniel-kantor.com>
2023-09-09 12:47:49 +0200

    docs: change gif theme

--- asciinema/export_gifs.sh
+++ asciinema/export_gifs.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 for cast_file in *.cast; do
     echo "Exporting $cast_file"
     base_name="${cast_file%.*}"
-    agg "$cast_file" "../assets/$base_name.gif"
+    agg "$cast_file" "../assets/$base_name.gif" --theme=monokai --font-size=18
     cp -f "../assets/$base_name.gif" "../docs/$base_name.gif"
     cp -f "../assets/$base_name.gif" "../docs/assets/$base_name.gif"
 done
