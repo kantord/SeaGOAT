ab07fc56e2818de73e1ef8790bcc143e0f995eb4
Daniel Kantor <git@daniel-kantor.com>
2023-10-10 09:34:34 +0200

    chore: add locustfile

---
+++ locustfile.py
@@ -0,0 +1,65 @@
+# pylint: disable=too-few-public-methods
+import random
+
+from locust import between
+from locust import HttpUser
+from locust import task
+
+WORDS = [
+    "function",
+    "where",
+    "display",
+    "print",
+    "handle",
+    "test",
+    "component",
+    "user",
+    "basket",
+    "execute",
+    "compute",
+    "store",
+    "api",
+    "endpoint",
+    "request",
+    "response",
+    "mobile",
+    "document",
+    "interface",
+    "web",
+    "database",
+    "source",
+    "deploy",
+    "library",
+    "framework",
+    "method",
+    "protocol",
+    "route",
+]
+
+
+def random_phrase():
+    phrase_length = random.randint(3, 15)
+    return " ".join(random.choices(WORDS, k=phrase_length))
+
+
+class BaseUser(HttpUser):
+    @task(8)
+    def query_endpoint(self):
+        phrase = random_phrase()
+        self.client.get(f"/query/{phrase}")
+
+
+class ShortWaitUser(BaseUser):
+    wait_time = between(0.3, 1.2)
+
+    @task(4)
+    def execute_tasks_short_wait(self):
+        self.query_endpoint()
+
+
+class LongWaitUser(BaseUser):
+    wait_time = between(1.2, 15)
+
+    @task(6)
+    def execute_tasks_long_wait(self):
+        self.query_endpoint()
