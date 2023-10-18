8c5cfe8fb5ebb62d5830d6c55bccc1a35c64b000
Daniel Kantor <git@daniel-kantor.com>
2023-10-18 11:04:50 +0200

    chore: compare different runs in the benchmark
    * chore: compare all benchmark runs together
    
    * chore: show different benchmark table for each project
    
    * chore: add chance by line # charts to benchmark

--- benchmark/load_results.py
+++ benchmark/load_results.py
@@ -1,6 +1,9 @@
+import collections
 import json
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from yaml import dump
 from yaml import load
 
@@ -117,31 +120,31 @@ def load_results(test_run):
         return load(input_file, Loader=Loader)
 
 
-def get_percentage_of_queries_with_correct_results(results, engine):
+def get_percentage_of_queries_with_correct_results(row):
     total_queries = 0
     correct_results = 0
 
-    for example in results:
+    for example in row["Data"]:
         for query in example["queries"]:
             total_queries += 1
-            if query["positionOfCorrectResult"][engine] is not None:
+            if query["positionOfCorrectResult"][row["Engine"]] is not None:
                 correct_results += 1
 
-    return float(correct_results) / total_queries * 100
+    return float(correct_results) / total_queries
 
 
-def get_percentage_of_examples_with_correct_results(results, engine):
+def get_percentage_of_examples_with_correct_results(row):
     total_examples = 0
     correct_results = 0
 
-    for example in results:
+    for example in row["Data"]:
         total_examples += 1
         for query in example["queries"]:
-            if query["positionOfCorrectResult"][engine] is not None:
+            if query["positionOfCorrectResult"][row["Engine"]] is not None:
                 correct_results += 1
                 break
 
-    return float(correct_results) / total_examples * 100
+    return float(correct_results) / total_examples
 
 
 def get_positions_of_correct_results(results, engine):
@@ -172,8 +175,8 @@ def get_best_position_of_correct_results(results, engine):
     return positions
 
 
-def get_average_position_of_a_correct_results(results, engine):
-    positions = get_positions_of_correct_results(results, engine)
+def get_average_position_of_a_correct_results(row):
+    positions = get_positions_of_correct_results(row["Data"], row["Engine"])
 
     if not positions:
         return None
@@ -181,22 +184,86 @@ def get_average_position_of_a_correct_results(results, engine):
     return sum(positions) / len(positions)
 
 
-def get_chance_of_getting_correct_result_in_n_lines(results, engine):
-    positions = {(index + 1): 0 for index in range(MAXIMUM_NUMBER_OF_RESULT_LINES)}
-    total_queries = 0
+def get_chance_of_getting_correct_result_in_n_lines(all_results, category_map):
+    positions = collections.defaultdict(
+        lambda: {(index + 1): 0 for index in range(MAXIMUM_NUMBER_OF_RESULT_LINES)}
+    )
+    total_queries = collections.defaultdict(lambda: 0)
 
-    for example in results:
-        for query in example["queries"]:
-            total_queries += 1
-            position = query["positionOfCorrectResult"][engine]
+    for test_run, results, engine in all_results:
+        for example in results:
+            for query in example["queries"]:
+                category_name = category_map(test_run, query, engine, example)
+                total_queries[category_name] += 1
+                position = query["positionOfCorrectResult"][engine]
 
-            if position is None:
-                continue
+                if position is None:
+                    continue
 
-            for index in range(position, MAXIMUM_NUMBER_OF_RESULT_LINES + 1):
-                positions[index] += 1
+                for index in range(position, MAXIMUM_NUMBER_OF_RESULT_LINES + 1):
+                    positions[category_name][index] += 1
+
+    category_names = list(total_queries.keys())
+    return [
+        [
+            [
+                float(positions[category_name][index + 1])
+                / total_queries[category_name]
+                for index in range(MAXIMUM_NUMBER_OF_RESULT_LINES)
+            ]
+            for category_name in category_names
+        ],
+        category_names,
+        [(index + 1) for index in range(MAXIMUM_NUMBER_OF_RESULT_LINES)],
+    ]
+
+
+def plot_chance_of_getting_correct_result_in_n_lines(all_results, category_map):
+    y_axes, labels, x_axis = get_chance_of_getting_correct_result_in_n_lines(
+        all_results, category_map
+    )
+    for y_axis, label in zip(y_axes, labels):
+        plt.plot(x_axis, y_axis, label=label)
+    plt.legend()
+    plt.xlabel("Position in result list")
+    plt.ylabel("Chance of the correct result")
+    plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1.0))
+    plt.show()
+
+
+def filter_minimum_example_quality(results, minimum_example_quality):
+    return [
+        result
+        for result in results
+        if result["qualityScore"] >= minimum_example_quality
+    ]
+
+
+def load_results_for_all_runs(minimum_example_quality):
+    list_of_test_runs = get_list_of_test_runs()
+    results = [
+        ("random", load_results(list_of_test_runs[0]), "random"),
+        (list_of_test_runs[0], load_results(list_of_test_runs[0]), "seagoat"),
+    ]
+
+    for test_run in list_of_test_runs[1:]:
+        results.append((test_run, load_results(test_run), "seagoat"))
 
     return [
-        float(positions[index + 1]) / total_queries * 100
-        for index in range(MAXIMUM_NUMBER_OF_RESULT_LINES)
+        [
+            test_run_name,
+            filter_minimum_example_quality(results, minimum_example_quality),
+            engine,
+        ]
+        for test_run_name, results, engine in results
     ]
+
+
+def get_list_of_projects():
+    list_of_test_runs = get_list_of_test_runs()
+    projects = set()
+
+    for example in load_results(list_of_test_runs[0]):
+        projects.add(example["repo"]["name"])
+
+    return sorted(list(projects))
