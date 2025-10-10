64ecdd427ee23c04b28b4ec2456022d323cb8292
Daniel Kantor <git@daniel-kantor.com>
2025-10-10 23:29:54 +0200

    chore: add empty rust project

--- seagoat/utils/wait.py
+++
@@ -1,9 +0,0 @@
-import time
-
-
-def wait_for(condition_function, timeout, period=0.05):
-    start_time = time.time()
-    while not condition_function():
-        if time.time() - start_time > timeout:
-            raise TimeoutError("Timeout expired while waiting for condition.")
-        time.sleep(period)
