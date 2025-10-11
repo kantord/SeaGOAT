24a1c2a9963c6677183e9dd250b8ab8f0d8a56fb
Daniel Kantor <git@daniel-kantor.com>
2025-10-11 19:57:03 +0200

    improve tests

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -36,3 +36,44 @@ async fn e2e_query_returns_hello_world() -> anyhow::Result<()> {
     server_task.abort();
     Err(anyhow::anyhow!("server did not respond: {:?}", last_err))
 }
+
+#[tokio::test]
+async fn e2e_queries_across_multiple_dbs() -> anyhow::Result<()> {
+    let dbs = seagoat::initialize_example_databases().await?;
+    let app = seagoat::build_router(seagoat::AppState { dbs });
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await?;
+    let addr = listener.local_addr()?;
+
+    let server_task = tokio::spawn(async move {
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    let client = reqwest::Client::new();
+    for id in seagoat::default_database_ids() {
+        let enc = id.replace('/', "%2F");
+        let url = format!("http://{}/v1/query?path={}", addr, enc);
+
+        // poll a few times in case server not ready yet
+        let deadline = tokio::time::Instant::now() + std::time::Duration::from_secs(3);
+        let json = loop {
+            if tokio::time::Instant::now() > deadline {
+                server_task.abort();
+                anyhow::bail!("server did not respond in time for {}", id);
+            }
+            match client.get(&url).send().await {
+                Ok(resp) if resp.status().is_success() => {
+                    let json: serde_json::Value = resp.json().await?;
+                    break json;
+                }
+                _ => tokio::time::sleep(std::time::Duration::from_millis(20)).await,
+            }
+        };
+
+        assert_eq!(json["hello_count"], serde_json::json!(2), "db={}", id);
+        assert!(json["tables"].as_array().unwrap().contains(&serde_json::json!("hello")), "db={}", id);
+    }
+
+    server_task.abort();
+    Ok(())
+}
